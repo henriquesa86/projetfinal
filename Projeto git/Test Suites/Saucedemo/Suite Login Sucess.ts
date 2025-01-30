c659e4c33503eda4efb7195cdff8d8b3b1f183e8
Henrique SÂa <henrique.sa@askblue.com>
2025-01-30 12:17:39 +0000

    primeircommit
    asddasdsa

---
+++ Projeto git/Test Suites/Saucedemo/Suite Login Sucess.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite Login Sucess</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>69664e34-1549-4f17-92ec-7f941c89a560</testSuiteGuid>
+   <testCaseLink>
+      <guid>653349e7-8c10-4797-8ec6-abab8f848829</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Saucedemo/Login positive</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
